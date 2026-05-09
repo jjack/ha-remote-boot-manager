e9c8e8a487396a7f05b9aa968ed58caf54d483d1
jeremy <jeremy@rocketscientry.com>
2026-05-09 03:15:55 +0000

    fix: correcting import name

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -4,7 +4,8 @@ from http import HTTPStatus
 from unittest.mock import AsyncMock, MagicMock
 
 from aiohttp import web
-from custom_components.grub_os_selector.webhook import async_validate_webhook_payload
+
+from custom_components.grubstation.webhook import async_validate_webhook_payload
 
 
 async def test_validate_webhook_empty_body():
